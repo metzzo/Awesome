f2674b1faad6f56a54ec0d095e214e7d3c2479da
Robert Fischer <robert@programming-with-design.at>
2014-04-19 17:42:34 +0200

    Added: AstSpec & Ast Structure Added: OPERATOR AstNode Added: INT_LITERAL AstNode

---
+++ spec/language/astSpec.js
@@ -0,0 +1,71 @@
+define(['app/compiler/ast/ast'], function(astModule) {
+  describe('AstNode', function() {
+    // this node mocks a node for testing purpose only
+    var stubNode = {
+      name: 'Stub',
+      params: { },
+      functions: {
+        
+      }
+    };
+    
+    var stubCount = 0;
+    beforeEach(function() {
+      stubCount = 0;
+    });
+    
+    describe('Operator', function() {
+      it('is created properly', function() {
+        // arrange
+        var ast;
+        var expectedAst = {
+          name: 'Operator',
+          params: {
+            leftOperand: {
+              name: 'Stub',
+              params: { }
+            },
+            rightOperand: {
+              name: 'Stub',
+              params: { }
+            },
+            operator: null
+          }
+        };
+        
+        // act
+        ast = astModule.createNode(astModule.AstPrototypes.OPERATOR, {
+          leftOperand: astModule.createNode(stubNode),
+          rightOperand: astModule.createNode(stubNode),
+          operator: null
+        });
+        
+        // assert
+        expect(ast).not.toBeNull();
+        expect(ast).toEqual(expectedAst);
+      });
+    });
+    
+    describe('Int Literal', function() {
+      it('is created properly', function() {
+        // arrange
+        var ast;
+        var expectedAst = {
+          name: 'Int Literal',
+          params: {
+            value: 42
+          }
+        };
+        
+        // act
+        ast = astModule.createNode(astModule.AstPrototypes.INT_LITERAL, {
+          value: 42
+        });
+        
+        // assert
+        expect(ast).not.toBeNull();
+        expect(ast).toEqual(expectedAst);
+      });
+    });
+  });
+});
---
+++ src/app/compiler/ast/ast.js
@@ -0,0 +1,56 @@
+define([ ], function() {  
+  return {
+    createNode: function(astPrototype, params) {
+      if (!params) params = { };
+      if (!astPrototype) throw 'Invalid Parameter';
+      
+      return Object.create({ }, {
+        name: {
+          value: astPrototype.name,
+          enumerable: true,
+          writeable: false
+        },
+        params: {
+          value: params,
+          enumerable: true,
+          writeable: true
+        },
+        functions: {
+          value: astPrototype.functions,
+          enumerable: false,
+          writeable: false
+          
+        },
+        traverse: {
+          value: function() {
+            
+          },
+          enumerable: false,
+          writeable: false
+        }
+      });
+    },
+    AstPrototypes: {
+      OPERATOR: {
+        name: 'Operator',
+        params: {
+          leftOperand: null,
+          rightOperand: null,
+          operator: null
+        },
+        functions: {
+          
+        }
+      },
+      INT_LITERAL: {
+        name: 'Int Literal',
+        params: {
+          intValue: 0
+        },
+        functions: {
+          
+        }
+      }
+    }
+  };
+});
